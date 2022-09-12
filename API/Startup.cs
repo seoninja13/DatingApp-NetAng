852832cef1967ac1bfa3790a72745999908c3d92
seoninja13 <dachevivo@gmail.com>
2022-09-12 11:39:36 -0700

    End of section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,8 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,6 +17,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace API
@@ -25,16 +31,12 @@ namespace API
           
         }
 
-       // public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(options => {
-               options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
-            services.AddCors();
-
+           services.AddApplicationServices(_config);
+            services.AddIdentityServices(_config);
+ services.AddCors();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -57,6 +59,7 @@ namespace API
             app.UseRouting();
 
             app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+           app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
